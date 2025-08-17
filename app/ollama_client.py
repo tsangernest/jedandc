cd8dd36cf1b257dccba12050a83fb4e7a6a20fec
p14s <tsangernest@hotmail.com>
2025-08-17 11:30:26 -0500

    * update requirements again because ironpdf is paid * implemented ability to send pdf to gemma and have it respond with some stuff working...

---
+++ app/ollama_client.py
@@ -0,0 +1,57 @@
+import ollama
+import sys
+from PIL import Image
+import pymupdf
+from pydantic import BaseModel
+
+from pprint import pprint
+
+
+DOCS_DIR: str = f"../data"
+
+
+class Receipt(BaseModel):
+    attributes: dict
+
+
+def query_ollama():
+    doc_receipt = pymupdf.open(filename=f"{DOCS_DIR}/receipt-low-film.pdf")
+
+    if not doc_receipt.page_count:
+        print(f"\nNo Image found! Bailing hard")
+        sys.exit(0)
+
+    name = (
+        doc_receipt
+        .name
+        .rsplit(sep="/", maxsplit=1)[-1]
+        .split(sep=".", maxsplit=1)[0]
+    )
+    for page in doc_receipt:
+        pic_receipt = page.get_pixmap()
+        pic_receipt.save(f"{DOCS_DIR}/{name}.png")
+
+    print(f"\n***\nQuerying Ollama\n***\n")
+    content = "Describe image as accurately as possible. Use and lean on OCR more than yourself."
+
+    format_req = ".\nReturn as JSON. without newline characters."
+    response = ollama.chat(
+        model="gemma3:12b",
+        format=Receipt.model_json_schema(),
+        messages=[{
+            "role": "user",
+            "content": f"{content}{format_req}",
+            "images": [f"{DOCS_DIR}/receipt-low-film.png",],
+        }],
+    )
+
+    print(f"\nOllama response")
+    ollama_response = Receipt.model_validate_json(response.message.content)
+    pprint(ollama_response)
+    print(f"\n{response['total_duration']=}\n\n")
+
+
+if __name__ == "__main__":
+    query_ollama()
+    print(f"\n***\nEnd of Processing\n***\n")
+
